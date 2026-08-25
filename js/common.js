b1779b62a75834b090305f6fc89a07d269c35d6f
neuronhospital <neuronhospitallatur@gmail.com>
2026-08-25 17:37:49 +0530

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ window.$=window.U?.$||((id)=>document.getElementById(id));
 if("serviceWorker"in navigator)window.addEventListener("load",()=>navigator.serviceWorker.register("./service-worker.js").catch(()=>{}));
 document.addEventListener("DOMContentLoaded",()=>{
  const p=document.body.dataset.title||"Portal",h=document.getElementById("header"),f=document.getElementById("footer");
- if(h)h.innerHTML=`<header class="site-header"><div class="container header-inner"><a class="brand" href="index.html"><div class="brand-mark">N</div><div><div class="brand-title">NEURON Hospital</div><div class="brand-sub">Latur • Pediatric Neurology & Epilepsy</div></div></a><a class="header-home" href="index.html">Home</a></div></header>`;
+ if(h)h.innerHTML=`<header class="site-header"><div class="container header-inner"><a class="brand" href="index.html"><img class="brand-logo" src="assets/neuron_logo.png" alt="NEURON Hospital Logo"><div><div class="brand-title">NEURON Hospital</div><div class="brand-sub">Latur • Pediatric Neurology & Epilepsy Center</div></div></a><a class="header-home" href="index.html"><img src="assets/icons/home.svg" alt="Home"></a></div></header>`;
  document.title=p+" | NEURON Hospital";
  if(f)f.innerHTML=`<footer class="footer"><div class="container"><div class="footer-nav">
 <a href="index.html"><img class="nav-icon" src="assets/icons/home.svg" alt="">Home</a>
