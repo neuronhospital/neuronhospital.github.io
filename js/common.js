eb4cbb8136f01536737e81e5f912b73478419cc7
neuronhospital <neuronhospitallatur@gmail.com>
2026-08-25 11:17:39 +0530

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -4,5 +4,12 @@ document.addEventListener("DOMContentLoaded",()=>{
  const p=document.body.dataset.title||"Portal",h=document.getElementById("header"),f=document.getElementById("footer");
  if(h)h.innerHTML=`<header class="site-header"><div class="container header-inner"><a class="brand" href="index.html"><div class="brand-mark">N</div><div><div class="brand-title">NEURON Hospital</div><div class="brand-sub">Latur • Pediatric Neurology & Epilepsy</div></div></a><a class="header-home" href="index.html">Home</a></div></header>`;
  document.title=p+" | NEURON Hospital";
- if(f)f.innerHTML=`<footer class="footer"><div class="container"><div class="footer-nav"><a href="index.html">Home</a><a href="opd_booking.html">OPD Booking</a><a href="eeg_booking.html">EEG Booking</a><a href="opd_update.html">Update OPD</a><a href="eeg_update.html">Update EEG</a><a href="statistics.html">Statistics</a></div><div class="footer-contact"><b>NEURON Hospital, Latur</b><br>Near Patil Plaza, Ausa Road, Latur</div><div class="version">24-08-2026-15:58:14-HTML78-v6-CODE78-v6</div></div></footer>`;
+ if(f)f.innerHTML=`<footer class="footer"><div class="container"><div class="footer-nav">
+<a href="index.html"><img class="nav-icon" src="assets/icons/home.svg" alt="">Home</a>
+<a href="opd_booking.html"><img class="nav-icon" src="assets/icons/opd-booking.svg" alt="">OPD Booking</a>
+<a href="eeg_booking.html"><img class="nav-icon" src="assets/icons/eeg-booking.svg" alt="">EEG Booking</a>
+<a href="opd_update.html"><img class="nav-icon" src="assets/icons/opd-update.svg" alt="">Update OPD</a>
+<a href="eeg_update.html"><img class="nav-icon" src="assets/icons/eeg-update.svg" alt="">Update EEG</a>
+<a href="statistics.html"><img class="nav-icon" src="assets/icons/statistics.svg" alt="">Statistics</a>
+</div><div class="footer-contact"><b>NEURON Hospital, Latur</b><br>Near Patil Plaza, Ausa Road, Latur</div><div class="version">24-08-2026-16:34:00-HTML78-v8-CODE78-v6</div></div></footer>`;
 });
