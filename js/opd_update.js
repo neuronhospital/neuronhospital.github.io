f9708cf2f09b3ae29631d30a08e50b0d0e2a761e
neuronhospital <neuronhospitallatur@gmail.com>
2026-08-30 14:21:49 +0530

    Add files via upload

--- js/opd_update.js
+++ js/opd_update.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     $("load").className="btn btn-primary";
     $("loadMessage").hidden=false;
     try{
-      const r=await NeuronAPI.call("getEEGPatientsByWhatsApp",{whatsapp:U.phone($("wa").value),city:$("city").value});
+      const r=await NeuronAPI.call("getFollowupPatientsByWhatsApp",{whatsapp:U.phone($("wa").value),city:$("city").value});
       r.patients.forEach(x=>{
         const b=document.createElement("button");
         b.className="patient-option";
